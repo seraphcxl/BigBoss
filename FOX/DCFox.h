81fbe515465f9c1f382caf8576c9ed4375a7bc1c
seraphcxl <seraph.cxl@gmail.com>
2014-08-01 20:23:07 +0800

    Update

---
+++ FOX/DCFox.h
@@ -0,0 +1,15 @@
+//
+//  DCFox.h
+//  FOX
+//
+//  Created by Derek Chen on 8/1/14.
+//  Copyright (c) 2014 Derek Chen. All rights reserved.
+//
+
+#ifndef FOX_DCFox_h
+#define FOX_DCFox_h
+
+#import "DCCoreDataStore.h"
+#import "DCCoreDataStoreManager.h"
+
+#endif
