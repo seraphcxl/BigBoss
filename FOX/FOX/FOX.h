5088b0f0b2ca796a653b8ed8210af9700fd7d9db
seraphcxl <seraph.cxl@gmail.com>
2014-08-01 15:43:04 +0800

    Add FOX

---
+++ FOX/FOX/FOX.h
@@ -0,0 +1,13 @@
+//
+//  FOX.h
+//  FOX
+//
+//  Created by Derek Chen on 8/1/14.
+//  Copyright (c) 2014 Derek Chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FOX : NSObject
+
+@end
---
+++ FOX/FOX/FOX.m
@@ -0,0 +1,13 @@
+//
+//  FOX.m
+//  FOX
+//
+//  Created by Derek Chen on 8/1/14.
+//  Copyright (c) 2014 Derek Chen. All rights reserved.
+//
+
+#import "FOX.h"
+
+@implementation FOX
+
+@end
